f71be55d88fdc442623ebe858b2229f178cce594
jborrellizara@gmail.com <jborrellizara@gmail.com>
2025-10-17 12:46:56 -0300

    Feature: Quito funciones que no corresponden a OVS

--- RDFGraphGPT/index.py
+++ RDFGraphGPT/index.py
@@ -15,30 +15,54 @@ from RDFGraphGPT.preguntas_ovs import PreguntaOVS
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
+nombre_grafo = "grafo_la_plata_mini"
+nombre_grafo_instancia = "grafo_lp_con_nueva_inst"
+texto_area_value = """
+    _ Terreno en venta ubicado en el barrio semi cerrado La Emilia sobre calle 132 Bis entre 474 y 476  (City Bell, La Plata, Buenos Aires) Con disposición interna y orientación al sur.
+    _ De medidas regulares, cuenta con 9mts de frente por 22mts de fondo, contabilizando una superficie total aproximada de 208mts2.
+    _ Acceso por calle de tierra mejorada y con los servicios de electricidad, agua corriente y gas natural disponibles en el lugar.
+    Yacoub | Construimos Confianza
+    Innovación | Calidad | Sustentabilidad | Diseño
+    70 Mil Metros Cuadrados | 23 Torres.
 
+"""
 
-@app.route("/", methods=["GET", "POST"])
-def graph():
-    if request.method == "POST":
-        # Obtiene los datos del formulario
-        form_data = request.form
-        text = form_data.get("text")
-        place = "DIFFERENT"
-        file_name = form_data.get("file-name")
-        svg_url = url_for("static", filename="archivo.svg")
+@app.route("/", methods=["GET"])
+def ovs_home():
+    files = get_files_in_directory("results")
+    rdf_text = search_file(nombre_grafo)
+    
+    place = "SAME"
+    svg_url = url_for('static', filename='archivo.svg')
+    exception = gff(nombre_grafo)
 
-        exception = generate_graph(text, place, file_name)
+    if exception:
+        return render_template('edit.html', rdf_text=rdf_text, error=exception, files=files)
+    else:
+        return render_template('ovs_home.html', graph=svg_url)
 
-        if exception:
-            rdf_text = search_file(file_name)
-            files = get_files_in_directory("results")
-            return render_template(
-                "edit.html", rdf_text=rdf_text, error=exception, files=files
-            )
-        else:
-            return render_template("graph.html", graph=svg_url)
+# @app.route("/", methods=["GET", "POST"])
+# def graph():
+#     if request.method == "POST":
+#         # Obtiene los datos del formulario
+#         form_data = request.form
+#         text = form_data.get("text")
+#         place = "DIFFERENT"
+#         file_name = form_data.get("file-name")
+#         svg_url = url_for("static", filename="archivo.svg")
 
-    return render_template("index.html")
+#         exception = generate_graph(text, place, file_name)
+
+#         if exception:
+#             rdf_text = search_file(file_name)
+#             files = get_files_in_directory("results")
+#             return render_template(
+#                 "edit.html", rdf_text=rdf_text, error=exception, files=files
+#             )
+#         else:
+#             return render_template("graph.html", graph=svg_url)
+
+#     return render_template("index.html")
 
 
 @app.post("/save-rdf")
@@ -107,28 +131,24 @@ def graph_from_file():
     return render_template("from_file.html", files=files)
 
 
-@app.route("/ovs_new_instance", methods=["GET", "POST"])
+@app.route("/ovs_new_instance", methods=["GET","POST"])
 def ovs_new_instance():
     if request.method == "POST":
-        # Obtiene los datos del formulario
-        form_data = request.form
-        text = form_data.get("text")
-        place = "DIFFERENT"
-        file_name = form_data.get("file-name")
-        svg_url = url_for("static", filename="archivo.svg")
-
-        exception = generate_ovs_graph(text, place, file_name)
-
+        place = "SAME" 
+        
+        file_name = nombre_grafo_instancia
+        svg_url = url_for('static', filename='archivo.svg')
+        
+        exception = gff(file_name);
+        
         if exception:
             rdf_text = search_file(file_name)
             files = get_files_in_directory("results")
-            return render_template(
-                "edit.html", rdf_text=rdf_text, error=exception, files=files
-            )
+            return render_template('edit.html', rdf_text=rdf_text, error=exception, files=files)
         else:
-            return render_template("graph.html", graph=svg_url)
-
-    return render_template("ovs_new_instance.html")
+            return render_template('graph.html', graph=svg_url)
+        
+    return render_template('ovs_new_instance.html', nombre_grafo=nombre_grafo, text_area_value=texto_area_value)
 
 
 @app.route("/questions", methods=["GET", "POST"])
